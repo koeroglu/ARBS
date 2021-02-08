b36677eaca4147defb0db7c7d27374982d62eb1a
koeroglu <78650428+koeroglu@users.noreply.github.com>
2021-02-08 18:51:06 +0200

    Update and rename larbs.sh to arbs.sh

--- arbs.sh
+++ arbs.sh
@@ -15,7 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/koeroglu/archrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/koeroglu/LARBS/master/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/koeroglu/ARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
